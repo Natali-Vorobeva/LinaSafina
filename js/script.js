b77ec108636ba82643e42a4521f0056db0a4266c
Natalia Vorobjeva <vorobjeva.natalia76@yandex.ru>
2022-10-30 16:25:07 +0300

    feat/menu

--- js/script.js
+++ js/script.js
@@ -1,55 +1,15 @@
-// $('.pic').bind('click', function() {
-// 	$('.pic').toggleClass('away');
-// 	$(this).removeClass('away').toggleClass('active');
-// });
+const menuBurger = document.querySelector('.menu-burger');
+const burgerOpen = document.querySelector('.burger');
+const menuBurgerClose = document.querySelector('.menu-burger__button-image');
 
-new Swiper('.image-slider', {
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev'
-	},
-	grabCursor: true,	
-	mousewheel: {
-		sensitivity: 1
-	},
+function showMenu() {
+	menuBurger.classList.add('menu_opened');	
+	}
+
+	function closeMenu() {
+		menuBurger.classList.remove('menu_opened');
+	}
+
 	
-	autoHeight: true,
-	slidesPerView: 1,
-	spaceBetween: 30,
-	// slidePerGroup: 1,
-	centeredSlides: true,
-	autoplay: {
-		// delay: 3000,
-		// stopOnLastSlide: true,
-	},
-	touchAngle: 90,
-	speed: 1000,
-	// effect: 'fade',
-	// fadeEffect: {
-		
-	// 	crossFade: true
-	// },
-	preloadImages: false,
-	lazy: {
-		loadOnTransitionStart: false,
-		loadPrevNext: false,
-	},
-	// zoom: {
-	// 	maxRatio: 3,
-	// 	minRatio: 1,
-	// },
-	// direction: 'vertical',
-	loop: true,
-	// loopedSlides: 3,
-	// breakpoints: {
-	// 	320: {
-	// 		slidesPerView: 1,
-	// 	},
-	// 	480: {
-	// 		slidesPerView: 2,
-	// 	},
-	// 	992: {
-	// 		slidesPerView: 3,
-	// 	},
-	// }
-});
+	menuBurgerClose.addEventListener('click', closeMenu);	
+	burgerOpen.addEventListener('click', showMenu);
